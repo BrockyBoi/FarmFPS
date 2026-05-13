c6cb4f090b1d861155ce94139ef0115e8cf3bfbc
Brock Soicher <brocksoicher@msn.com>
2026-05-12 22:10:42 -0400

    Added Basic Audio/Music and Bug Fixes

---
+++ Source/FarmFPS/Private/ExplodableResourcePickupActor.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Brock
+#include "ResourcePickupActor.h"
+
+// UE
+#include "CoreMinimal.h"
+
+// Generated
+#include "ExplodableResourcePickupActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AExplodableResourcePickupActor : public AResourcePickupActor
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/FarmFPS/Private/WaterMelonPickupActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ResourcePickupActor.h"
+#include "WaterMelonPickupActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AWaterMelonPickupActor : public AResourcePickupActor
+{
+	GENERATED_BODY()
+	
+};
